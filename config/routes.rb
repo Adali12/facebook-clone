e33460f251488281c2b4946365ac3334df1fd82b
adali12 <abayiringira1994@yahoo.com>
2019-08-27 08:16:09 -0700

    facebook2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :sessions
   resources :clones
+  root'sessions#new'
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
