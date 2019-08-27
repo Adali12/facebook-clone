23c3ab614f96e6961bd742714f0b0a06ebf991bc
adali12 <abayiringira1994@yahoo.com>
2019-08-27 08:35:30 -0700

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  
   resources :posts
   root 'sessions#new'
   resources :sessions
   resources :clones
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
