98b310dd9bfc0fb7f2dc2235e9b7f404d7aab783
adali12 <abayiringira1994@yahoo.com>
2019-08-30 02:25:13 -0700

    init123

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :clone
   mount_uploader :image, ImageUploader
+  validates :content, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :clones
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
